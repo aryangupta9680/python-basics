92c067ca2d3d7f6ab357d5abab553830ddef2c54
aryangupta9680 <aryan.gupta9680@gmail.com>
2025-05-25 10:01:44 +0530

    Add part01 in chapter04 folder

---
+++ Chapter04/part01.py
@@ -0,0 +1,15 @@
+# Strings: 
+
+# Program to take a string input from the user and print the string, its type, and its id
+
+# Take input from the user
+user_input = input("Enter a string: ")
+
+# Print the entered string
+print("You entered:", user_input)
+
+# Print the type of the variable
+print("Type of the input:", type(user_input))
+
+# Print the id of the variable
+print("id:", id(user_input))
